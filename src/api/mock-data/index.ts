3d6c436c00b957c0e8dc6d7da72216dfd89fae83
怕浪小乌龟 <wjh87851743@163.com>
2023-03-18 23:52:55 +0800

    ✨ feat: uuid

--- src/api/mock-data/index.ts
+++ src/api/mock-data/index.ts
@@ -1,6 +1,9 @@
-const arrayData = [
+import { uuid } from "@/utils";
+let kk =
+  "闭月羞花 沉鱼落雁 出水芙蓉 明眸皓齿 国色天香 鹤发童颜 雍容华贵 眉清目秀 威风凛凛 蓬头垢面 老态龙钟 弱不禁风 骨瘦如柴 面黄肌瘦 其貌不扬";
+export const arrayData = [
   {
-    group_id: 123,
+    group_id: uuid(),
     group_name: "test",
     group_type: "test",
     create_date: Date.now,
